6da02e5de05ef746f2bfb56254da4f7aad16cb9d
unknown <subodhdhungel20@gmail.com>
2023-04-26 22:05:29 +0545

    initial commit

---
+++ counterApp/index.js
@@ -0,0 +1,44 @@
+let buttons = document.querySelectorAll('button')
+let currOper = document.querySelector('input')
+let operatorButtons = document.querySelectorAll('button[value="+"],button[value="-"],button[value="x"],button[value="/"]')
+let equalsButton = document.querySelector('button[value="="]')
+let firstOper = ''
+let operator = ''
+let secondOper = ''
+
+buttons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        if(button.value === 'c'){
+            currOper.value = ''
+        }else if(button.value === 'del'){
+            currOper.value = currOper.value.slice(0, -1)
+        }else{
+            currOper.value += button.value
+        }
+    })
+})
+
+operatorButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        firstOper = currOper.value
+        operator = button.value
+    })
+})
+
+equalsButton.addEventListener('click', ()=>{
+    let result
+    let secondOper = currOper.value
+    if(operator === '/' && firstOper !== '' && secondOper !== ''){
+        result = Number.parseInt(firstOper) / Number.parseInt(secondOper)
+        currOper.value = result
+    }else if(operator === 'x' && firstOper !== '' && secondOper !== ''){
+        result = Number.parseInt(firstOper) * Number.parseInt(secondOper)
+        currOper.value = result
+    }else if(operator === '+' && firstOper !== '' && secondOper !== ''){
+        result = Number.parseInt(firstOper) + Number.parseInt(secondOper)
+        currOper.value = result
+    }else if(operator === '-' && firstOper !== '' && secondOper !== ''){
+        result = Number.parseInt(firstOper) - Number.parseInt(secondOper)
+        currOper.value = result
+    }
+})
